7aef0288362de562d845a51677307a84a59b7374
Wesley Rodrigues da Silva <wesley.it@gmail.com>
2017-10-19 18:20:51 -0200

    Fixed the cluster init script

--- docker/start_app.sh
+++ docker/start_app.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-cd /home/elixir/
+cd /home/elixir/elixir_docker_autocluster
 export ip="$(grep $(hostname) /etc/hosts | cut -f 1)"
 echo "Starting the local node..."
 mkfifo /tmp/elixir
-elixir --name "runner@$ip" --cookie cookie app.exs 1> /tmp/elixir 2> /tmp/elixir &
+iex --name "runner@$ip" --cookie cookie \
+    -S mix clusterize_as_hell 1> /tmp/elixir 2> /tmp/elixir &
 echo "Done!"
-
 cat /tmp/elixir
